779330f1ed45781034fafe079991329ee3a2d017
karthikkumar0125 <karthik89211@gmail.com>
2021-06-17 12:58:30 +0530

    added posts models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class post(models.Model):
+    #user=models.ForeignKey(user)
+    post_name=models.CharField(max_length=100)
+    post_data=models.TextField()
+    create_time=models.DateTimeField(auto_now_add=True)
+    votes=models.IntegerField(null=True, default=0)
+    #community=models.ForeignKey(community)
+
+
+class comments(models.Model):
+    #user=models.ForeignKey(user)
+    comment_data=models.CharField(max_length=255)
+    create_time=models.DateTimeField(auto_now_add=True)
+    votes=models.IntegerField(null=True, default=0)
+    post=models.ForeignKey(post, on_delete=models.CASCADE)
+
