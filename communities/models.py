3db892bd6d79c62e178f031b7c62e644017bd7c9
karthikkumar0125 <karthik89211@gmail.com>
2021-06-17 16:22:26 +0530

    added communities model and removed user

--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Community(models.Model):
+    name=models.CharField(max_length=50)
+    created_user=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now_add=True)
--- posts/models.py
+++ posts/models.py
@@ -1,21 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from communities.models import Community
 
 # Create your models here.
 
 
-class post(models.Model):
-    #user=models.ForeignKey(user)
+class Post(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     post_name=models.CharField(max_length=100)
     post_data=models.TextField()
     create_time=models.DateTimeField(auto_now_add=True)
     votes=models.IntegerField(null=True, default=0)
-    #community=models.ForeignKey(community)
+    community=models.ForeignKey(Community, on_delete=models.CASCADE)
 
 
-class comments(models.Model):
-    #user=models.ForeignKey(user)
+class Comments(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     comment_data=models.CharField(max_length=255)
     create_time=models.DateTimeField(auto_now_add=True)
     votes=models.IntegerField(null=True, default=0)
-    post=models.ForeignKey(post, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
 
